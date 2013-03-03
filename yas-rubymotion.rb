9d2646b2ad58ac620d71339fa634b41629122dc3
ainame <ainame954@facebook.com>
2013-03-04 01:34:20 +0900

    fork repo and edit some code for RubyMotion

--- yas-rubymotion.rb
+++ yas-rubymotion.rb
@@ -1,17 +1,17 @@
-#!/usr/bin/ruby
-
 require 'time'
 require 'optparse'
+require 'active_support/core_ext/string'
 
 helpdoc = <<END_HELP_DOC
 
-yasobjc is a tool for yasnippet snippet generation for cocoa standard library.
+yas-rubymotion.rb is a tool for yasnippet snippet generation for cocoa standard library which using in RubyMotion.
+*This script depends on the ActiveSupport. Please install the 'activesupport' gem.
 
-Basic Usage: yasobjc.rb -o DIR file1 file 2 ...
+Basic Usage: ruby yas-rubymotion.rb -o DIR file1 file 2 ...
 
 -o DIR: The output directory for the snippets generated
 
-Example: find /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.0.sdk/System/Library/Frameworks -name "*.h" | xargs yasobjc.rb -o ~/.emacs.d/yasnippet/text-mode/cc-mode/objc-mode
+Example: find /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.1.sdk/System/Library/Frameworks -name "*.h" | xargs ruby yas-rubymotion.rb -o  ~/.emacs.d/yasnippet/ruby-mode
 
 which will generate all snippets into standard yasnippet directory.
 
@@ -20,48 +20,10 @@ One snippet is generated per objective-c function, and the snippets are categori
 It's recommended to use ETAGS plus auto-complete library for better completion experience.
 
 END_HELP_DOC
-
-
-$spaces = /[\t ]*/
-$word = /[_a-zA-Z0-9]+/
-$type = /\((#{$spaces}#{$word}){1,3}#{$spaces}\**#{$spaces}\)/
-$return_type = /#{$type}?/
-$function_name = /#{$word}/
-$param_type = /#{$type}/
-$param_name = /#{$word}/
-$param = /(#{$function_name})#{$spaces}:#{$spaces}(#{$param_type})#{$spaces}(#{$param_name})/
-$function = /#{$spaces}[+-]#{$spaces}#{$return_type}#{$spaces}((#{$param}#{$spaces})+)/ 
-
-
-def transform_function(definition, header_file_basename, output_dir)
-  full_function_name = ""
-  result = ""
-  index = 1
-  
-  definition.scan($param).each do |param_match|
-    full_function_name << ":" if not full_function_name.empty?
-    full_function_name << param_match[0]
-    result << " " if not result.empty?
-    result << param_match[0] << ':'  << "${#{index}:#{param_match[1]}#{param_match[3]}}"
-    index += 1
-  end
-
-  output_filename = full_function_name.gsub(/:/, '-')
-  output_full_filename = File.expand_path(File.join(output_dir, output_filename))
-
-  open(output_full_filename, 'w') do |f|
-    f << '#name : ' << full_function_name << "\n"
-    f << '#group : ' << header_file_basename << "\n"
-    f << '# --' << "\n"
-    f << result
-  end	
-
-end
-
 options = {}
 
 optparse = OptionParser.new do|opts|
-  opts.banner = "Usage: yasobjc -o DIR file1 file2 ..."
+  opts.banner = "Usage: ruby yas-rubymotion.rb -o DIR file1 file2 ..."
   
   options[:verbose] = false
   opts.on( '-v', '--verbose', 'Output more information' ) do
@@ -81,6 +43,54 @@ end
 
 optparse.parse!
  
+$spaces = /[\t ]*/
+$word = /[_a-zA-Z0-9]+/
+$type = /\((#{$spaces}#{$word}){1,3}#{$spaces}\**#{$spaces}\)/
+$return_type = /#{$type}?/
+$function_name = /#{$word}/
+$param_type = /#{$type}/
+$param_name = /#{$word}/
+$param = /(#{$function_name})#{$spaces}:#{$spaces}(#{$param_type})#{$spaces}(#{$param_name})/
+$function = /#{$spaces}[+-]#{$spaces}#{$return_type}#{$spaces}((#{$param}#{$spaces})+)/ 
+
+$function_format = <<FUNC_FORMAT
+def %s(%s)
+  $0
+end
+FUNC_FORMAT
+
+
+def transform_function(definition, header_file_basename, output_dir)
+  full_function_name = ""
+  result = []
+
+  definition.scan($param).each do |param_match|
+    full_function_name << ":" if not full_function_name.empty?
+    full_function_name << param_match[0]
+
+    # MacRuby or RubyMotion can accept lowercamel variable name
+    if result.empty?
+      result << param_match[0]
+      result << param_match[3].camelize(:lower)
+    else
+      result << "#{param_match[0].camelize(:lower)}:#{param_match[3].camelize(:lower)}"
+    end
+  end
+
+  output_filename = full_function_name.gsub(/:/, '-')
+  output_full_filename = File.expand_path(File.join(output_dir, output_filename))
+  
+  function_definition = sprintf $function_format, result[0], result[1..-1].join(", ")
+
+  open(output_full_filename, 'w') do |f|
+    f << '#name : ' << full_function_name << "\n"
+    f << '#group : ' << header_file_basename << "\n"
+    f << '# --' << "\n"
+    f << function_definition
+  end	
+
+end
+
 puts "Start generation..." if options[:verbose]
 start_time = Time.now
 snippets_count = 0
@@ -92,7 +102,12 @@ ARGV.each do|f|
   puts "Processing #{basename}.h" if options[:verbose]
   
   open(File.expand_path(f), 'r').each do |line|
-    function_match = $function.match(line)
+    function_match = $function.match(
+      line.force_encoding("UTF-8").encode(
+        "UTF-16BE", :invalid => :replace,
+        :undef => :replace, :replace => '?'
+      ).encode("UTF-8")
+    )
 
     if function_match
       transform_function(function_match[2], basename, options[:outputdir])
@@ -104,4 +119,3 @@ end
 end_time = Time.now
 secs = (end_time - start_time)
 puts "#{snippets_count} snippets generated in #{secs} seconds.\n" if options[:verbose]
-
